894ce6a75bd7d7ddde05836a45fe77712bc08991
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2024-04-25 16:02:58 +0300

    Learning Routing in Angular
    1. Started to learn Routing in Angular.
    2. התחלתי ראוטינג בפעם המי יודע כמה
    3. אני עדיין לא ממש מבין את הנושא

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import {FirstComponent} from './first/first.component';
 import { Routes } from '@angular/router';
+import {SecondComponent} from './second/second.component';
 
 export const routes: Routes = [];
